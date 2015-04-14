12d952a4d66c053f05257e9584eb65349d7e0473
zwilloughby <lps.freakz@gmail.com>
2015-04-14 17:20:22 -0500

    Create Flower.js
    This adds a flower or takes away a flower when a life is gained, or the cactus runs into oil.

---
+++ Flower.js
@@ -0,0 +1,26 @@
+var flower = true; 
+var playing = true;
+
+
+background(219, 242, 255);
+
+        if(flower){
+            if(playing){
+                }
+        }
+        
+        
+        if(flower===true){
+            
+            noStroke ();
+            fill(255, 204, 252);
+            ellipse(186, 146, 27, 27);
+            ellipse(210, 147, 27, 27);
+            ellipse(219, 128, 27, 27);
+            ellipse(211, 111, 27, 27);
+            ellipse(187, 111, 27, 27);
+            ellipse(177, 130, 27, 27);
+            fill(237, 232, 128);
+            ellipse(199, 129, 30, 30);
+
+        }
