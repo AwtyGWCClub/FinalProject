9a583df233c32528b907ef4bf81f2c1961635195
ahpartridge <apartridge@awty.org>
2015-04-14 17:25:15 -0500

    Create statusbar.js

---
+++ statusbar.js
@@ -0,0 +1,31 @@
+noStroke();
+fill(0, 0, 0);
+rect(306,214,196,309);
+background(201, 238, 247);
+fill(70, 184, 87);
+ellipse(353,306,-14,220);
+ellipse(353,202,12,11);
+arc(337,248,24,-24,154,389);
+rect(325,222,12,28);
+ellipse(331,220,12,12);
+
+arc(369,248,24,-24,154,389);
+
+rect(369,221,12,28);
+
+ellipse(375,220,12,12);
+
+rect(347,203,12,102);
+
+fill(232, 222, 172);
+ellipse(308,374,270,106);
+fill(250, 239, 182);
+ellipse(198, 384, 730, 40);
+fill(255, 255, 0);
+ellipse(307,95,75,75); 
+fill(255, 255, 255);
+rect(20,20,200,15);
+fill(162, 183, 235);
+rect(20,20,random(20,220),15);
+
+///Alex and Abby are trying to create the status bar using the concept of pixels and percentages. However, we do not know how. Our background was 8000x600 pixels. Our screen will be 800x600. The Status Bar is 200 pixels wide. So for every 10 pixels that the cactus moves, the blue rectangle on the status bar will move 2 pixels.////
